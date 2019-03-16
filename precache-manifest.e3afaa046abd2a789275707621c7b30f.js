1133545ea639684005bd52935cdac4a837a70f58
Aman Jaiswal <antooaman@gmail.com>
2019-03-16 14:36:47 +0530

    Updates

---
+++ precache-manifest.e3afaa046abd2a789275707621c7b30f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.e4bf40f2.js"
+  },
+  {
+    "revision": "7bfa56f18053e5f90dc2",
+    "url": "/contactmanager/static/js/main.7bfa56f1.chunk.js"
+  },
+  {
+    "revision": "58e15e81ed0aba53123d",
+    "url": "/contactmanager/static/js/2.58e15e81.chunk.js"
+  },
+  {
+    "revision": "7bfa56f18053e5f90dc2",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "58e15e81ed0aba53123d",
+    "url": "/contactmanager/static/css/2.d4e0dee9.chunk.css"
+  },
+  {
+    "revision": "889c242c644f7493f665553219cb7dfc",
+    "url": "/contactmanager/index.html"
+  }
+];
